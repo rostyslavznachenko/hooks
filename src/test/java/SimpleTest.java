e5acbac048f7662275094615d67c30a480457649
rznachenko <rostislav.znachenko@dataart.com>
2018-05-04 11:28:11 +0300

    Add simple test

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SimpleTest {
+    @Test
+    public void testingHooksIntegrationWithJenkins() {
+        System.setProperty("webdriver.chrome.driver", ".//src//main//resources//chromedriver.exe");
+        WebDriver browser = new ChromeDriver();
+        browser.manage().window().setSize(new Dimension(1920,1080));
+        browser.get("https://www.google.com/");
+        WebElement searchInput = new WebDriverWait(browser, 10)
+                .until(ExpectedConditions.visibilityOfElementLocated(By.id("lst-ib")));
+        String searchValue = "testing hooks";
+        String valueToFailTest = "fail";
+        searchInput.sendKeys(Keys.chord(searchValue, Keys.ENTER));
+        System.out.println(browser.getTitle());
+        Assert.assertTrue(browser.getTitle().contains(valueToFailTest));
+        browser.quit();
+    }
+}
